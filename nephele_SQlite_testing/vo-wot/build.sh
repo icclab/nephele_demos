692685825bce1afde228bdd2293f845e2fbbd7ce
miltzhaw <miltzhaw@zhaw.ch>
2024-06-11 15:42:36 +0200

    VO and nginx deployed in same pod for shared volume

--- nephele_SQlite_testing/vo-wot/build.sh
+++ nephele_SQlite_testing/vo-wot/build.sh
@@ -6,7 +6,7 @@
 # Authors:      Leonardo Militano
 # Date:         2024-03-23
 ################################################################################
-export IMAGE_NAME=robopaas/nephele-vo:latest
+export IMAGE_NAME=robopaas/nephele-vo:test
 
 # Get this script's path
 pushd `dirname $0` > /dev/null
