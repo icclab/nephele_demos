2bac08511d76be16f3fb79959aec38f829348428
miltzhaw <miltzhaw@zhaw.ch>
2024-11-12 12:04:55 +0100

    updated vo version zenoh

--- cVO_summit_drone/foxglove-zenoh-bridges/run_bridge.sh
+++ cVO_summit_drone/foxglove-zenoh-bridges/run_bridge.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-source /root/colcon_ws/install/setup.bash
+source /home/ros/colcon_ws/install/setup.bash
+source /opt/ros/humble/setup.bash
+#source /root/colcon_ws/install/setup.bash
 ros2 launch foxglove_bridge foxglove_bridge_launch.xml &
 BRIDGE_PID=$!
 
 sleep 30
 
 #./zenoh-bridge-ros2dds -c zenoh-bridge-conf.json5 &
-zenoh-bridge-ros2dds -c zenoh-bridge-conf.json5 &
+zenoh-bridge-ros2dds -c /zenoh-bridge-conf.json5 &
 ZENOH_PID=$!
 
 wait $BRIDGE_PID
