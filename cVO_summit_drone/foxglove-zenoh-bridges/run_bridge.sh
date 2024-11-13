a2919cb3ec18467ab8937c010ab7d6d9cd00ba74
miltzhaw <miltzhaw@zhaw.ch>
2024-11-13 09:09:29 +0100

    minor updates

--- cVO_summit_drone/foxglove-zenoh-bridges/run_bridge.sh
+++ cVO_summit_drone/foxglove-zenoh-bridges/run_bridge.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-source /home/ros/colcon_ws/install/setup.bash
 source /opt/ros/humble/setup.bash
+source /home/ros/colcon_ws/install/setup.bash
+
 #source /root/colcon_ws/install/setup.bash
+#ros2 launch foxglove_bridge foxglove_bridge_launch.xml asset_uri_allowlist:="['^package://(?:\\w+/)*\\w+\\.(?:dae|fbx|glb|gltf|jpeg|jpg|mtl|obj|png|stl|tif|tiff|urdf|webp|xacro)$', '^package://summit_xl_description/.+\\.(?:dae|fbx|glb|gltf|jpeg|jpg|mtl|obj|png|stl|tif|tiff|urdf|webp|xacro)$']" &
 ros2 launch foxglove_bridge foxglove_bridge_launch.xml &
 BRIDGE_PID=$!
 
