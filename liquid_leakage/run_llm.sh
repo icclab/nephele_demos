baa487774e9ceb57d9ffccb0389a3c259f8d9836
Leonardo Militano <milt@zhaw.ch>
2025-02-04 16:03:03 +0100

    run correct service script

--- liquid_leakage/run_llm.sh
+++ liquid_leakage/run_llm.sh
@@ -3,7 +3,7 @@ source /opt/ros/humble/setup.bash
 source /root/ros2_ws/install/setup.bash
 
 
-ros2 run leakage leakage_server &
+python3 /root/ros2_ws/src/LLM_service/leakage/scripts/ros2service.py  &
 LLMSERVICE_PID=$!
 
 sleep 1
