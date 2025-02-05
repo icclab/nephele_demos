903ec8529997a82f8d999666ea565231f2f838e7
Giovanni Toffetti <toff@zhaw.ch>
2025-02-05 14:15:55 +0000

    update sleep time

--- liquid_leakage/run_llm.sh
+++ liquid_leakage/run_llm.sh
@@ -6,16 +6,17 @@ source /root/ros2_ws/install/setup.bash
 python3 /root/ros2_ws/src/LLM_service/leakage/scripts/ros2service.py  &
 LLMSERVICE_PID=$!
 
-sleep 1
+sleep 8m
 
-
-python3 /root/ros2_ws/src/LLM_service/leakage/scripts/ros2client.py  &
-LLMCLIENT_PID=$!
-
-sleep 1
 
 zenoh-bridge-ros2dds -c /zenoh-bridge-conf.json5 &
 ZENOH_PID=$!
+
+
+sleep 1s 
+
+python3 /root/ros2_ws/src/LLM_service/leakage/scripts/ros2client.py  &
+LLMCLIENT_PID=$!
 
 wait $LLMSERVICE_PID
 wait $LLMCLIENT_PID
