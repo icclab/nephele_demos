ce04f0179a01db5936a6285d488f4090903483fe
Sisqui <aries94leifu@gmail.com>
2025-04-22 10:32:43 +0200

    update drone mapping

--- cVO_summit_drone/drone-config-files/drone-mavros.py
+++ cVO_summit_drone/drone-config-files/drone-mavros.py
@@ -8,6 +8,8 @@ from sensor_msgs.msg import BatteryState
 from geometry_msgs.msg import Point
 from visualization_msgs.msg import Marker, MarkerArray
 from rclpy.qos import QoSProfile, ReliabilityPolicy, DurabilityPolicy
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.wait_for_message import wait_for_message
 
 import subprocess
 import time
@@ -23,59 +25,92 @@ import signal
 
 process_bringup = None
 
-def read_from_sensor():  
-    battery_percent = None 
-    gps_data = []
-    local_data = []
+class BatteryRead(Node):
+    def __init__(self):
+        super().__init__('battery_read')
 
-    class BatteryRead(Node):
-        def __init__(self):
-            super().__init__('battery_read')
-            
-            qos_profile = QoSProfile(reliability=ReliabilityPolicy.BEST_EFFORT, durability=DurabilityPolicy.VOLATILE, depth=10)
+    #     qos_profile = QoSProfile(
+    #         reliability=ReliabilityPolicy.BEST_EFFORT,
+    #         durability=DurabilityPolicy.VOLATILE,
+    #         depth=10
+    #     )
 
-            self.subscription = self.create_subscription(BatteryState, '/mavros/battery', self.battery_callback, qos_profile)
-            self.gps_subscription = self.create_subscription(MarkerArray, '/gps_marker', self.gps_callback, 10)
-            self.localPostion_subscription = self.create_subscription(MarkerArray, '/local_marker', self.local_callback, 10)
+    #     self.battery_percent = None
+    #     self.gps_data = []
+    #     self.local_data = []
 
-        def battery_callback(self, msg):
-            nonlocal battery_percent
-            if msg.voltage < 13.6:
+    #     self.subscription = self.create_subscription(
+    #         BatteryState, '/mavros/battery', self.battery_callback, qos_profile)
+
+    #     self.local_position_subscription = self.create_subscription(
+    #         Marker, '/local_marker', self.local_callback, 10)
+        
+    #     self.gps_subscription = self.create_subscription(
+    #         Marker, '/gps_marker', self.gps_callback, 10) 
+
+    # def battery_callback(self, msg):
+    #     if msg.voltage < 13.6:
+    #         self.battery_percent = 0
+    #     elif msg.voltage == 16.8: 
+    #         self.battery_percent = 100
+    #     else: 
+    #         self.battery_percent = ((msg.voltage - 13.6) / (16.8 - 13.6)) * 100
+    #     self.get_logger().info(f'Battery: {self.battery_percent:.2f}%')
+
+    # def gps_callback(self, msg):
+    #     self.gps_data = [msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]
+
+    # def local_callback(self, msg):
+    #     # self.get_logger().info(f"Received local marker message: {msg}")
+    #     self.local_data = [msg.pose.position.x, msg.pose.position.y, msg.pose.position.z]
+
+def read_from_sensor():
+    rclpy.init()
+    battery_read = BatteryRead()
+    print("Reading data from drone...")
+
+    battery_percent, gps_data, local_data = 0, [], []
+
+    # Spin for 2 seconds
+    # t_end = time.time() + 2.5
+    try:
+        # while time.time() < t_end:
+            # print(time.time())
+        # rclpy.spin_once(battery_read, timeout_sec=0.5)
+        
+        # Battery
+        msg = wait_for_message(BatteryState, battery_read, "/mavros/battery", time_to_wait=0.5)
+        if msg[0] :
+            battery_percent = msg[1].voltage
+            if msg[1].voltage < 13.6:
                 battery_percent = 0
-            elif msg.voltage == 16.8: 
+            elif msg[1].voltage == 16.8: 
                 battery_percent = 100
             else: 
-                battery_percent = ((msg.voltage - 13.6) / (16.8 - 13.6)) * 100
-
-        def gps_callback(self, msg):
-            nonlocal gps_data
-            for marker in msg.markers:
-                point = Point(
-                    x=marker.pose.position.x,
-                    y=marker.pose.position.y,
-                    z=marker.pose.position.z
-                )
-                gps_data.append(point)
-
-        def local_callback(self, msg):
-            nonlocal local_data
-            for marker in msg.markers:
-                point = Point(
-                    x=marker.pose.position.x,
-                    y=marker.pose.position.y,
-                    z=marker.pose.position.z
-                )
-                local_data.append(point)
-
-    def main():
-        rclpy.init()
-        battery_read = BatteryRead()
-        rclpy.spin_once(battery_read, timeout_sec=1.0)
+                battery_percent = ((msg[1].voltage - 13.6) / (16.8 - 13.6)) * 100
+        # GPS
+        msg = wait_for_message(Marker, battery_read, "/local_marker", time_to_wait=1.5)
+        if msg[0] :
+            gps_data = [msg[1].pose.position.x, msg[1].pose.position.y, msg[1].pose.position.z]
+        else :
+            print("No GPS data")
+        # Local data
+        msg = wait_for_message(Marker, battery_read, "/gps_marker", time_to_wait=1.5)
+        if msg[0] :
+            local_data = [msg[1].pose.position.x, msg[1].pose.position.y, msg[1].pose.position.z]
+        time.sleep(0.1)
+    finally:
         battery_read.destroy_node()
         rclpy.shutdown()
+    print("Done!")
+    # executor = MultiThreadedExecutor()
+    # executor.add_node(battery_read)
+    # executor.spin()  # Run in a multi-threaded mode
 
-    main()
-
+    # battery_read.destroy_node()
+    # rclpy.shutdown()
+    
+    # return battery_read.battery_percent, battery_read.gps_data, battery_read.local_data
     return battery_percent, gps_data, local_data
     
 battery_percent, gps_data, local_data = read_from_sensor()
@@ -86,22 +121,44 @@ allAvailableResources_init = {
     'local_data': local_data
 }
 
-possibleLaunchfiles_drone_init = ['startmapping_drone', 'bringup_zed', 'stopzed_drone', '3dmap_drone', 'savemap_drone', 'savebag_drone', 'stopbag_drone']
+possibleLaunchfiles_drone_init = ['startmapping_drone', 'bringup_zed', 'stopzed_drone', 'save3dmap_drone', 'savemap_drone', 'savebag_drone', 'stopbag_drone']
 mapdataExportTF_init = [True, False]
 
 def get_map_as_string(map_file_path):
     try:
         # Read the PGM file as binary
         with open(map_file_path, 'rb') as file:
-            pgm_data = file.read()
+           pgm_data = file.read()
 
         # Convert the PGM binary data to a string
         pgm_string = base64.b64encode(pgm_data).decode('utf-8')
-
+        # # Open the file in binary mode and read it in chunks
+        # with open(map_file_path, 'rb') as file:
+        #     chunks = []
+        #     while chunk := file.read(8192):  # Read in 8KB chunks
+        #         chunks.append(base64.b64encode(chunk).decode('utf-8'))
+        
+        # # Join all the encoded chunks into a single string
+        # png_string = ''.join(chunks)
         return pgm_string
 
     except FileNotFoundError:
         print("Error: Map file not found.")
+        return None
+
+def get_3dmap_as_string(map_file_path):
+    try:
+        # Read the bag file as binary
+        with open(map_file_path, 'rb') as file:
+            chunks = []
+            while chunk := file.read(8192):  # Read in 8KB chunks
+                chunks.append(base64.b64encode(chunk).decode('utf-8'))
+        
+        # Join all the encoded chunks into a single string
+        png_string = ''.join(chunks)
+        return png_string
+    except FileNotFoundError:
+        print("Error: Bagfile not found.")
         return None
     
 def get_rosbag_as_string(bag_file_path):
@@ -135,6 +192,7 @@ async def triggerBringup_drone_handler(params):
     bringupaction = None
     stopzedaction = None
     mappingaction = None
+    mapping3dsaveaction = None
     saveaction = None
     savebagaction = None
     stopbagaction = None
@@ -180,7 +238,7 @@ async def triggerBringup_drone_handler(params):
     if launchfileId == 'startmapping_drone':
         # If battery percentage is more than 50, allow to start the mapping launch file
         # print("Battery sufficient, start drone mapping!")
-        process_mapping = subprocess.Popen(['ros2', 'launch', 'pc2_to_grid', 'pc2_transformed_to_grid_launch.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process_mapping = subprocess.Popen(['ros2', 'launch', 'pc2_to_grid', 'grid_mapping_launch.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         time.sleep(5) 
 
         if process_mapping.poll() is None:
@@ -190,16 +248,16 @@ async def triggerBringup_drone_handler(params):
             print("Failed to start mapping.")
             mappingaction = False
     
-    if launchfileId == '3dmap_drone':
-        process_mapping = subprocess.Popen(['ros2', 'launch', 'bonxai_ros', 'bonxai_mapping.launch.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if launchfileId == 'save3dmap_drone':
+        process_mapping = subprocess.Popen(['ros2', 'launch', 'pc2_to_grid', 'pc2_map_png_launch.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         time.sleep(5) 
 
         if process_mapping.poll() is None:
-            print("Mapping started successfully.")
-            mappingaction = True
+            print("3D Mapping saved successfully.")
+            mapping3dsaveaction = True
         else:
-            print("Failed to start mapping.")
-            mappingaction = False
+            print("Failed to save the 3D map.")
+            mapping3dsaveaction = False
 
     if launchfileId == 'savemap_drone': #and mappingaction == True:
         print("Mapping finished, save the map!")
@@ -263,7 +321,15 @@ async def triggerBringup_drone_handler(params):
          return {'result': stopbagaction, 'message': f'Your {launchfileId} is in progress!'}
     elif launchfileId == 'stopzed_drone':
         return {'result': stopzedaction, 'message': f'Your {launchfileId} is in progress!'}
+    elif launchfileId == 'save3dmap_drone':
+        return {'result': mapping3dsaveaction, 'message': f'Your {launchfileId} is in progress!'}
     
+async def map3dExport_drone_handler(params):
+    params = params['input'] if params['input'] else {}
+    map_file_path = '/home/orin/my_3dmap.png'
+    map_string = get_3dmap_as_string(map_file_path)
+    return map_string
+
 async def mapExport_drone_handler(params):
     params = params['input'] if params['input'] else {}
     map_file_path = '/home/orin/my_map.pgm'
--- cVO_summit_drone/drone-config-files/start.sh
+++ cVO_summit_drone/drone-config-files/start.sh
@@ -15,13 +15,21 @@ screen -S drone -p zenoh -X exec zenoh-bridge-ros2dds -c /home/orin/nephele_demo
 sleep 1s
 
 #In the same container we will also start the zed
-#screen -S drone -X screen -t zed
-#screen -S drone -p zed -X exec ros2 launch zed_wrapper zed_camera.launch.py
-sleep 1s
+# screen -S drone -X screen -t zed
+# screen -S drone -p zed -X exec ros2 launch zed_wrapper zed_camera.launch.py
+# sleep 1s
 
-#In the same container we will also start the vo-wot environment
+# #In the same container we will also start the vo-wot environment
 screen -S drone -X screen -t vo-wot
 screen -S drone -p vo-wot -X exec vo-wot -t /home/orin/nephele_demos/cVO_summit_drone/drone-config-files/drone-td.json -f /home/orin/nephele_demos/cVO_summit_drone/drone-config-files/drone.yaml /home/orin/nephele_demos/cVO_summit_drone/drone-config-files/drone-mavros.py
+
+#In the same container we will also start markerarray publisher
+# screen -S drone -X screen -t marker
+# screen -S drone -p vo-wot -X exec python3 /home/orin/ros2_ws/src/pc2_to_grid/pc2_to_grid/leakage.py
+
+#In the same container we will also start the fake data
+# screen -S drone -X screen -t fake
+# screen -S drone -p vo-wot -X exec python3 /home/orin/ros2_ws/src/pc2_to_grid/pc2_to_grid/test.py
 
 
 #Attach to screen
